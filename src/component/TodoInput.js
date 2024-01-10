4ba4affb63a4c27d5214468ffc8d5aa25a9ba272
seon <sunkyoung.dev@gmail.com>
2024-01-10 11:24:37 +0900

    3. TODOINPUT 조건부 랜더링

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {MdAdd} from "react-icons/md";
 import './scss/TodoInput.scss';
+import cn from 'classnames';  //class add/remove 도와주는
 
 const TodoInput = () => {
+    //useState는 렌더링 상태를 관리하는 변수지정 리엑트 훅
+    const [open,setOpen]=useState(false);
+
+    //버튼 클릭 이벤트
+    const onToggle=e=>{
+        setOpen(!open);
+
+    }
+
     return (
         <>
-            <div className='form-wrapper'>
+            {open && (<div className='form-wrapper'>
                 <form className='insert-form'>
                     <input
                         type='text'
                         placeholder='할 일을 입력 후, 엔터를 누르세요!'
                     />
                 </form>
-            </div>
-            <button className='insert-btn'>
+            </div>)}
+            {/*
+            cn() : 첫번째 파라미터는 항상 유지할 클래스
+                    두번째 파라미터는 논리 상태값
+                    => 논리상태값이 true일경우 해당클래스가 추가
+                       false일 경우 제거
+            */}
+            <button className={cn('insert-btn',{open})} onClick={onToggle}>
                 <MdAdd/>
             </button>
         </>
