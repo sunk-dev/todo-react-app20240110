cacb5312c7794696bd3dc2e00ef1da20ebd8d173
seon <sunkyoung.dev@gmail.com>
2024-01-10 11:43:25 +0900

    4. 동적랜더링

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {MdDelete, MdDone} from "react-icons/md";
 import './scss/TodoItem.scss';
-const TodoItem = () => {
+const TodoItem = ({item}) => {
+    const {id,title,done}=item;
+    // console.log(props)
     return (
         <li className='todo-list-item'>
             <div className='check-circle'>
-                <MdDone/>
+                {done&& <MdDone/>}
             </div>
-            <span className='text'>할 일 어쩌구~~</span>
+            <span className='text'>{title}</span>
             <div className='remove'>
                 <MdDelete/>
             </div>
--- src/component/TodoMain.js
+++ src/component/TodoMain.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import {MdDelete, MdDone} from "react-icons/md";
+
 import './scss/TodoMain.scss';
 import TodoItem from "./TodoItem";
-const TodoMain = () => {
-    return (
-        <ul class='todo-list'>
-           <TodoItem/>
-           <TodoItem/>
-           <TodoItem/>
 
+const TodoMain = () => {
+
+    const todoList = [
+        {
+            id: 1,
+            title: '장보기',
+            done: false
+        },
+        {
+            id: 2,
+            title: '저녁먹기',
+            done: true
+        },
+        {
+            id: 3,
+            title: '수다떨기',
+            done: false
+        },
+        {
+            id: 4,
+            title: '축구하기',
+            done: false
+        }
+
+    ];
+
+    return (
+        <ul className='todo-list'>
+            {
+                //동적랜더링
+                //map을 돌릴때 key값을 꼭 넣어주기
+                todoList.map(todo=><TodoItem key={todo.id} item={todo}/>)
+            }
         </ul>
     );
 };
